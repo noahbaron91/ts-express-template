0c3b8099f4e8ae84649fd8bd72e6069fbdef74bf
Noah Baron <32229274+noahbaron91@users.noreply.github.com>
2024-10-11 16:58:10 -0700

    feat: express 8080

--- src/index.ts
+++ src/index.ts
@@ -6,7 +6,7 @@ app.get('/', (req, res) => {
   res.send('Hello World!');
 });
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 8080;
 
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
